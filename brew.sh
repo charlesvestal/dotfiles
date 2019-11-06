5a02312388a9a2776ad455a268cfc8fff3a85e82
Charles Vestal <charlesv@gmail.com>
2019-11-06 12:32:23 +0100

    moving osxfuse to home

--- brew.sh
+++ brew.sh
@@ -24,9 +24,6 @@ brew cask install obinslab-starter
 brew cask install private-internet-access
 brew cask install figma
 
-#brew cask install osxfuse
-#curl https://rclone.org/install.sh | sudo bash
-
 brew install mackup
 
 brew cleanup
