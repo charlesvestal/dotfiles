ee0953fbe54ed387051524cf1331691ad682ff9e
Charles Vestal <charlesv@gmail.com>
2019-11-07 14:18:56 +0100

    adding some stuff for home only

--- home.sh
+++ home.sh
@@ -3,13 +3,13 @@ brew cask install filebot
 
 #TODO get filebot license and put in ~/Downloads
 
-#TODO set up filebot parsing (maybe keep rpi?)
+#TODO set up filebot parsing (maybe keep on rpi?)
 # TODO add to cron:
 #   filebot -script fn:amc --output ~/mount/putio -non-strict --def clean=y subtitles=en artwork=y --conflict auto ~/mount/putio/_To\ Rename
-#
+#   filebot -script fn:amc --output ~/mount/putio -non-strict --def clean=y subtitles=en artwork=y --conflict auto ~/mount/putio/Movies
 #   filebot -script fn:configure
 #
-#   filebot -script fn:artwork.tvdb ~/mount/putio/TV\ Shows
+#   filebot -script ~/Library/Application\ Support/Filebot/Scripts/artwork.tvdb.groovy ~/mount/putio/TV\ Shows
 #   filebot -script fn:artwork.tmdb ~/mount/putio/Movies
 #   filebot -script fn:suball ~/mount/putio/Movies -non-strict --def
 #   filebot -script fn:suball ~/mount/putio/TV\ Shows -non-strict --def
@@ -17,5 +17,6 @@ brew cask install filebot
 # TODO add exclude list 
 
 curl https://rclone.org/install.sh | sudo bash
+rclone config # configure rclone putio 
 
 #TODO add putio rclone config and automount
