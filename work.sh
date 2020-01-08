a6d2b42ffc9832224f1f644ac22b83160552414a
Charles Vestal <charlesv@gmail.com>
2020-01-08 17:55:13 +0100

    moving app dev to commented out

--- work.sh
+++ work.sh
@@ -1,7 +1,6 @@
 echo "Installing work specific applications..."
 echo "..."
 
-brew install cocoapods
 
 brew tap phrase/brewed
 brew install phraseapp
@@ -16,6 +15,27 @@ brew cask install charles
 mas install 824171161   # Affinity Designer
 mas install 409203825   # Numbers
 mas install 409201541   # Pages
-mas install 497799835   # Xcode
+
+
+# echo "Installing iOS development software..."
+
+# mas install 497799835   # Xcode
+# brew install cocoapods
+
+# echo "Installing Android development software..."
+
+# brew cask install homebrew/cask-versions/adoptopenjdk8
+# brew cask install android-sdk
+
+# echo "export ANDROID_HOME=/usr/local/share/android-sdk" >> ~/.bash_profile
+# source ~/.bash_profile
+
+# echo "" > ~/.android/repositories.cfg
+# touch ~/.android/repositories.cfg
+# sdkmanager --update
+# sdkmanager "platform-tools" "platforms;android-28"
+
+# brew cask install android-studio        
+
 
 echo "Finished with work specific appliactions."
